f0675a46d3c7d052d0327d0b482a99e69ba4268e
RAVITEJA <127837720+ra816@users.noreply.github.com>
2026-06-21 11:26:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'ChangeMe123!')
+"
